21b062e4c3a40af56398c5eb2d7b719a58239bc0
JesusLover1337 <wilmer.lundberg@skola.taby.se>
2023-03-21 08:40:11 +0100

    23456u7ykhjmnc

---
+++ javascript.js
@@ -0,0 +1,53 @@
+let searchText = document.getElementById("txtSearch");
+
+searchText.onkeydown = async function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    removeJsDivs();
+    let searchTerm = searchText.value;
+    let results = await search(searchTerm);
+    renderResults(results);
+  }
+};
+
+function removeJsDivs() {
+  const jsDivs = document.querySelectorAll(".con");
+  console.log(jsDivs);
+  jsDivs.forEach((div) => div.remove());
+}
+
+async function search(searchString) {
+  let apiKey = "7f26946c05328556f6f899a4d314a61d";
+  var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`;
+  let response = await fetch(url);
+  let json = await response.json();
+  return json;
+}
+
+function renderResults(results) {
+  let allObjects = [];
+  allObjects = results.results;
+
+  for (let index = 0; index < allObjects.length; index++) {
+    let object = allObjects[index];
+    console.log("loopar igenom objekten ", object);
+    const myDiv = document.createElement("div");
+    const myText = document.createTextNode(object.original_title);
+    var a = document.createElement("a");
+    a.appendChild(myText);
+    a.title = "More information";
+    a.href = "index2.html";
+    const myImg = document.createElement("img");
+
+    const baseUrl = "https://image.tmdb.org/t/p/";
+    const posterSize = "w185";
+    const posterPath = object.poster_path;
+    myImg.src = `${baseUrl}${posterSize}${posterPath}`;
+
+    myDiv.style.fontSize = "large";
+    myDiv.style.textAlign = "center";
+    myDiv.appendChild(a);
+    myDiv.appendChild(myImg);
+    document.body.appendChild(myDiv).className = "con";
+  }
+}
